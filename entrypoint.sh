39a4471f8ddfe8a5052d7e6ac981511b4e96a6de
matthaywardwebdesign <matt@matthayward.com.au>
2019-01-23 11:04:25 +1100

    Simple test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
-mkdir genny-workspace
-ls
 cd genny-workspace && /upgrade.sh $GENNY_VERSION
 
 # Zip up the release
-zip -q -r release.zip genny-workspace
+zip -q -r /release.zip /genny-workspace/
 
 # Name the release
-mv release.zip release.$GENNY_VERSION.zip
+mv /release.zip /release.$GENNY_VERSION.zip
 
 # Upload the release
-s3cmd put release.$GENNY_VERSION.zip s3://genny-releases
+s3cmd put /release.$GENNY_VERSION.zip s3://genny-releases
